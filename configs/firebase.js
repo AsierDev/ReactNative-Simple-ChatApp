f5da1e9a288c85528cd562aa150a4080f427ee44
Asier <asierp.dev@gmail.com>
2018-08-25 16:20:15 +0200

    Setting up RNCA and expo

---
+++ configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBQACkWCMcTQO2041Tu6_e7ABK0NcftD5I",
+    authDomain: "simple-react-native-chat-app.firebaseapp.com",
+    databaseURL: "https://simple-react-native-chat-app.firebaseio.com",
+    projectId: "simple-react-native-chat-app",
+    storageBucket: "simple-react-native-chat-app.appspot.com",
+    messagingSenderId: "596759743109"
+    }
